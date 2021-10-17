7b9bf980685953b513c3b8e309de8b57b8c103a4
widianapw <widianaputraa@gmail.com>
2021-10-17 21:10:08 +0800

    first commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.error')
+@section('content')
+    <section class="section">
+        <div class="container mt-5">
+            <div class="page-error">
+                <div class="page-inner">
+                    <h1>404</h1>
+                    <div class="page-description">
+                        The page you were looking for could not be found.
+                    </div>
+                </div>
+            </div>
+            <div class="simple-footer mt-5">
+                Copyright &copy; Stisla 2018
+            </div>
+        </div>
+    </section>
+@endsection
